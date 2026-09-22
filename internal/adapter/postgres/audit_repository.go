34d7f6bbaa092619de766809fbbdfb9292b17b43
ezequielranieri <ez.ranieri@gmail.com>
2026-09-22 13:21:01 -0300

    fix: add missing pgtype import for pgtype types in audit_repository.go

--- internal/adapter/postgres/audit_repository.go
+++ internal/adapter/postgres/audit_repository.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	postgressqlc "github.com/ezequielranieri/agent-gateway/internal/adapter/postgres/sqlc"
